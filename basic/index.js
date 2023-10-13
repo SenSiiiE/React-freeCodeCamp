d903e1bb04b20e9c41a6a96bc0570c0d34f912e8
Vikash Sharma <ikassharma10112@gmail.com>
2023-10-13 20:03:31 +0530

    first commit

---
+++ basic/index.js
@@ -0,0 +1,46 @@
+// ReactDOM.render(<ul>Fruits <li>Apple</li><li>Orange</li></ul>, document.getElementById("root"));
+
+// function MainContent(){
+//     return (
+//         <ol><li>VIkash</li><li>Sapna</li></ol>
+//     )
+// }
+
+// ReactDOM.render(
+//     <div>
+//         <MainContent />
+//     </div>,
+//     document.getElementById("root")
+// );
+
+// const h1 = document.createElement("h3");
+// h1.textContent = "My name is VIkash";
+// h1.className = "name";
+// document.getElementById("root").append(h1);
+
+// JSX
+// const page = (
+//   <div>
+//     <h1>This is my blog</h1>
+//     <p>My name is VIkash</p>                        
+//   </div>
+// );
+// console.log(page);
+// ReactDOM.render(page, document.getElementById("root"));
+
+const navBar = (
+  <nav>
+    <h1>My Website</h1>
+    <ul>
+      <li>Home</li>
+      <li>Profile</li>
+      <li>Log Out</li>
+    </ul>
+  </nav>
+);
+
+console.log(navBar);
+
+// // ReactDOM.append(navBar, document.getElementById("root"));
+// document.getElementById("root").append(navBar);
+
